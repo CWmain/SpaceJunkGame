c309597f8cdfa359d0e70ab65ee88c144c3ae5bf
CWmain <cameronwhite2478@gmail.com>
2024-12-24 14:20:20 +1100

    Changed asteroid deletion from width height to an area calculation (not sure if it is accurate)

--- Objects/player_rocket/player_rocket.gd
+++ Objects/player_rocket/player_rocket.gd
@@ -6,7 +6,7 @@ const STOP_SPEED = 300.0
 const ROTATION_SPEED = PI/64
 const JUMP_VELOCITY = -100.0
 ## Variable to control how small an asteroid can get
-const MIN_ASTEROID_SIZE = 35
+const MIN_ASTEROID_SIZE = 1000
 
 @onready var cutting_tool = $CuttingTool
 @onready var collision_polygon = $CuttingTool/CollisionPolygon
@@ -26,7 +26,7 @@ func _physics_process(_delta):
 	#	velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Accelerate"):
+	if Input.is_action_pressed("Accelerate"):
 		velocity.y = JUMP_VELOCITY
 		velocity.x = 0
 		velocity = velocity.rotated(rotation)
@@ -61,24 +61,25 @@ func asteroidCut():
 
 		#NOTICE: If preformace issues consider object pooling for asteroids
 		for overlapping in intersection:
-			#region Localise Polygon and find its max height and width
-			var largeX: int = -9223372036854775808
-			var largeY: int = -9223372036854775808
-			var smallX: int = 9223372036854775807 
-			var smallY: int = 9223372036854775807 
+			#region Localise Polygon and find its area
+			var leftSide = 0
+			var rightSide = 0 
 			var localOverLapping = overlapping.duplicate()
-			for i in range(localOverLapping.size()):
+			for i in range(localOverLapping.size()):			
 				localOverLapping[i] = entity.to_local(localOverLapping[i])
-				smallX = min(smallX, localOverLapping[i].x)
-				smallY = min(smallY, localOverLapping[i].y)
-				largeX = max(largeX, localOverLapping[i].x)
-				largeY = max(largeY, localOverLapping[i].y)
+				
+				if (i > 0):
+					leftSide += localOverLapping[i].x * localOverLapping[i-1].y
+					rightSide += localOverLapping[i-1].x * localOverLapping[i].y
+				
 			#endregion	
-			
-			print("X: ", largeX-smallX, " Y: ", largeY-smallY)
+			leftSide += localOverLapping[0].x * localOverLapping[localOverLapping.size()-1].y
+			rightSide += localOverLapping[localOverLapping.size()-1].x * localOverLapping[0].y
+			var area = abs(leftSide-rightSide)/2
+			print("Area: ", area)
 			
 			#TODO: Play a shattering partile effect the emphasis destruction of asteroid
-			if (largeX-smallX < MIN_ASTEROID_SIZE or largeY-smallY < MIN_ASTEROID_SIZE):
+			if (area < MIN_ASTEROID_SIZE):
 				continue
 			
 			var splitAsteroid: Asteroid = asteroid.instantiate()
