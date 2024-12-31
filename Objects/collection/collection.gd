abd147538e24778c9610c2461990b0b7d4d78672
CWmain <cameronwhite2478@gmail.com>
2024-12-31 23:38:21 +1100

    Readded old method of scanning pixels to give me new values when texture changes. New viewport method of collecting asteroids, it creates a copy of the visual part and has it zeroed, than counts the pixels. It appears to work independently of screen size, futher testing is needed

--- Objects/collection/collection.gd
+++ Objects/collection/collection.gd
@@ -4,17 +4,30 @@ extends Node2D
 #TODO: Understand why the region has an odd offset
 var screenshotRegion = Rect2(135,165,300,300)
 
+@onready var sub_viewport = $SubViewportContainer/SubViewport
+
 #TODO: Update colour definitions when a new texture is made
 #region Asteroid Colors
-var red : Color = Color(0.5529, 0, 0, 1)
-var green : Color = Color(0, 0.4196, 0.0706, 1)
-var blue : Color = Color(0, 0.3216, 0.5529, 1)
+var red : Color = Color(1, 0, 0, 1)
+var green : Color = Color(0, 0.7569, 0.1255, 1)
+var blue : Color = Color(0, 0.5804, 1, 1)
 #endregion
 
 func _on_collection_hit_box_body_entered(body):
+	var toCopy: Polygon2D = body.visual
+	var vis : Polygon2D = Polygon2D.new()
+	vis.texture = toCopy.texture
+	vis.texture_offset = toCopy.texture_offset
+	vis.polygon = toCopy.polygon
+	
+	sub_viewport.add_child(vis)
+	vis.position = Vector2(150,150)
+	
 	await RenderingServer.frame_post_draw
 	print("Attempting to make image")
-	var myImage : Image = get_viewport().get_texture().get_image().get_region(screenshotRegion)
+	var myImage : Image = sub_viewport.get_texture().get_image()
+	
+	#printAllPixels(myImage)
 
 	var asteroidMakeUp: Dictionary = {"r":0,"g":0,"b":0}
 	for i in range(0,300):
@@ -31,9 +44,22 @@ func _on_collection_hit_box_body_entered(body):
 		print(key, ": ", asteroidMakeUp[key])
 
 	var screenshot = ImageTexture.create_from_image(myImage)
+	vis.queue_free()
 	text_screen_shot.texture = screenshot
+	
+func printAllPixels(myImage: Image):
+	var dict = {}
+	for i in range(0,300):
+		for j in range(0,300):
+			if dict.has(myImage.get_pixel(i,j)):
+				dict[myImage.get_pixel(i,j)] += 1
+			else:
+				dict[myImage.get_pixel(i,j)] = 1
+	for key in dict.keys():
+		if dict[key] > 200:
+			print(key, ": ", dict[key])
 
-static func cmpColors(x: Color, y: Color) -> bool:
+func cmpColors(x: Color, y: Color) -> bool:
 	if (x.a8 != y.a8):
 		return false
 	if (x.r8 != y.r8):
