d65943c673d1c1719100feead7f3b74fbf0442cf
CWmain <cameronwhite2478@gmail.com>
2024-12-31 21:30:55 +1100

    Currently have data of the asteroid comming out, issue with noise resolved by only counting pixels with a count over 200, need to work out which colors are which still

--- Objects/collection/collection.gd
+++ Objects/collection/collection.gd
@@ -9,7 +9,18 @@ func _on_collection_hit_box_body_entered(body):
 	await RenderingServer.frame_post_draw
 	print("Attempting to make image")
 	var myImage : Image = get_viewport().get_texture().get_image().get_region(screenshotRegion)
+	var dict : Dictionary = {}
 	
+	for i in range(0,300):
+		for j in range(0,300):
+			if dict.has(myImage.get_pixel(i,j)):
+				dict[myImage.get_pixel(i,j)] += 1
+			else:
+				dict[myImage.get_pixel(i,j)] = 1
+	for key in dict.keys():
+		if dict[key] > 200:
+			print(key, ": ", dict[key])
+
 	var screenshot = ImageTexture.create_from_image(myImage)
 	text_screen_shot.texture = screenshot
 
