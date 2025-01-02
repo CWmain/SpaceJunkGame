f1a623a2a98715ed96ca4011fe19b83aaa048a51
CWmain <cameronwhite2478@gmail.com>
2025-01-02 11:38:20 +1100

    Started implementation of custom resources for asteroids to allow for their easier creation initially

--- Objects/asteroid/asteroid.gd
+++ Objects/asteroid/asteroid.gd
@@ -7,6 +7,8 @@ const MIN_ASTEROID_SIZE = 1000
 @onready var hit_box : CollisionPolygon2D = $HitBox
 @onready var visual : Polygon2D = $Visual
 
+@export var test: AsteroidVisuals
+
 var area = 1001;
 
 #TODO: When created they should move away from the cutting tool, not sure where to place logic
@@ -15,9 +17,6 @@ var area = 1001;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_area(calculate_area())
-	
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -43,9 +42,9 @@ func remove_asteroid():
 	queue_free()
 
 func calculate_area():
-	var leftSide = 0
-	var rightSide = 0
-	var poly = visual.get_polygon()
+	var leftSide: float = 0
+	var rightSide: float = 0
+	var poly : PackedVector2Array = visual.get_polygon()
 	for i in range(poly.size()):				
 		if (i > 0):
 			leftSide += poly[i].x * poly[i-1].y
---
+++ Objects/asteroid/asteroid_visuals.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name AsteroidVisuals
+
+@export var shape: PackedVector2Array
+@export var textureOffest: Vector2 = Vector2(0,0)
+@export var textureRotation: float = 0
