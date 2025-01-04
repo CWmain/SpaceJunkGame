f1bda918f1f4793c99e290a49b3bf619be9162cf
CWmain <cameronwhite2478@gmail.com>
2025-01-04 16:27:24 +1100

    Made a basic screen curve shader that becomes more pronounced as speed increases, still needs alot of work to look good

--- Objects/camera_follower/camera_follower.gd
+++ Objects/camera_follower/camera_follower.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var followSpeed: float = 500.0
 @export var toFollow: Node2D
 
+@onready var color_rect = $CanvasLayer/ColorRect
+
 func _ready():
 	pass
 
@@ -13,20 +15,19 @@ func _physics_process(delta):
 	
 	# Normal Speed
 	if curDistance < maxDistance/2:
-		position.x = floorf(move_toward(floor(position.x), floor(toFollow.position.x), followSpeed*delta))
-		position.y = floorf(move_toward(floor(position.y), floor(toFollow.position.y), followSpeed*delta))
+		cameraSpeedMult(delta)
+		color_rect.material.set("shader_paramater/curve", float(10))
 	# Double Speed
 	elif curDistance < maxDistance:
-		position.x = floorf(move_toward(floor(position.x), floor(toFollow.position.x), followSpeed*delta*2))
-		position.y = floorf(move_toward(floor(position.y), floor(toFollow.position.y), followSpeed*delta*2))
+		cameraSpeedMult(2*delta)
+		color_rect.material.set("shader_parameter/curve", float(5))
 	# Keeps at the maximum distance no matter what	
 	else:
 		position += toFollow.position - position - (position.direction_to(toFollow.position)*maxDistance)
 		position = floor(position)
+		color_rect.material.set("shader_paramater/curve", float(1))
 
-		
-	pass
-
-##
-#func cameraSpeedMult(mult: float) -> void:
-	
+## Function to easily apply multiplications of follow speed
+func cameraSpeedMult(mult: float) -> void:
+	position.x = floorf(move_toward(position.x, toFollow.position.x, followSpeed*mult))
+	position.y = floorf(move_toward(position.y, toFollow.position.y, followSpeed*mult))
