b982dbdfee045ad147c0a0b64029b8b6d4c82ac0
CWmain <cameronwhite2478@gmail.com>
2025-01-05 16:11:12 +1100

    Add new star asteroid preset using the improved AsteroidProperties

--- Objects/asteroid/asteroid_spawner/asteroid_spawner.gd
+++ Objects/asteroid/asteroid_spawner/asteroid_spawner.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
 var asteroid = preload("res://Objects/asteroid/asteroid.tscn")
-var shape = preload("res://Objects/asteroid/base_asteroids/default.tres")
+var def = preload("res://Objects/asteroid/base_asteroids/default.tres")
+var star = preload("res://Objects/asteroid/base_asteroids/star.tres")
+
+#TODO: Consider moving random to singelton so seeds may be used and consistent
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _process(delta):
 
 	if get_tree().get_nodes_in_group("Asteroid").size() < 2:
 		var newAsteroid: Asteroid = asteroid.instantiate()
-		var newProperties: AsteroidProperties = shape.duplicate()
+		var newProperties: AsteroidProperties
+		
+		# We duplicate to avoid issues of editing the base shape
+		if rng.randf() > 0.5:
+			newProperties = def.duplicate()
+		else:
+			newProperties = star.duplicate()
+
 
 		get_tree().get_root().add_child(newAsteroid)
 
