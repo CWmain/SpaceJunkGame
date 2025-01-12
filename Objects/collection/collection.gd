11165f5e14db012c92b89e4c097f467c9814511a
CWmain <cameronwhite2478@gmail.com>
2025-01-12 11:40:45 +1100

    Moved to pixelCounting system to its own object as I want to count the pixels of the newly created asteroid and when it goes into the collector to collect a score

--- Objects/collection/collection.gd
+++ Objects/collection/collection.gd
@@ -1,38 +1,16 @@
 extends Node2D
-@onready var text_screen_shot = $ColorRect/TextScreenShot
-
-#region Variables for the pixel counting thread
-var pixelCountingThread: Thread
-var pixelCountingSemaphore: Semaphore
-var pixelCountingMutex: Mutex
-var exitThread: bool = false
-var imageToCount: Image
-#endregion
 
 var scoreMutex: Mutex
 var scoreUpdate: bool = false
 var score: Dictionary = {"r":0,"g":0,"b":0}
 
 @onready var collection_hit_box = $CollectionHitBox
-@onready var sub_viewport = $SubViewport
-
+@onready var pixel_counter = $pixelCounter
 @onready var scoreSign = $Sign
 
-#TODO: Update colour definitions when a new texture is made
-#region Asteroid Colors
-var red : Color = Color(1, 0, 0, 1)
-var green : Color = Color(0, 0.7569, 0.1255, 1)
-var blue : Color = Color(0, 0.5804, 1, 1)
-#endregion
+
 
 func _ready():
-	# Initialise all variables needed for the pixel counting thread
-	pixelCountingMutex = Mutex.new()
-	pixelCountingSemaphore = Semaphore.new()
-	pixelCountingThread = Thread.new()
-	pixelCountingThread.start(countRGBPixels)
-	exitThread = false
-	
 	scoreMutex = Mutex.new()
 
 func _process(_delta):
@@ -44,17 +22,7 @@ func _process(_delta):
 	if Input.is_action_just_pressed("PlacePoint"):
 		print(score)
 	
-func _exit_tree():
-	# Set exit condition to true.
-	pixelCountingMutex.lock()
-	exitThread = true 
-	pixelCountingMutex.unlock()
 
-	# Unblock by posting.
-	pixelCountingSemaphore.post()
-
-	# Wait until it exits.
-	pixelCountingThread.wait_to_finish()
 	
 func _on_collection_hit_box_body_entered(_body):
 	collectAsteroid()
@@ -67,62 +35,10 @@ func collectAsteroid() -> void:
 			continue
 		
 		var visualPoly: Polygon2D = ast.visual
-		visualPoly.reparent(sub_viewport)
-		visualPoly.position = Vector2(150,150)
+		pixel_counter.visualPolygonImageGenerator(visualPoly)
 		
-		await RenderingServer.frame_post_draw
-		print("Attempting to make image")
-		var myImage : Image = sub_viewport.get_texture().get_image()
-		
-		#printAllPixels(myImage)
-		pixelCountingMutex.lock()
-		imageToCount = myImage
-		pixelCountingMutex.unlock()
-		pixelCountingSemaphore.post()
-
-		# Reparents the visual polygon back to its original owner (Asteroid)
-		visualPoly.reparent(ast)
-		visualPoly.position = Vector2(0,0)
-
 		# Free the asteroid
 		ast.queue_free()
-
-		# Generates a screednshot for visual clarity on the subviewport
-		var screenshot = ImageTexture.create_from_image(myImage)
-		text_screen_shot.texture = screenshot
-
-## NOTE: Variable passed to countRGBPixels by the imageToCount global variable
-## Threaded function which returns the red, green, blue pixel count of imageToCount
-func countRGBPixels() -> void:
-	while true:
-		pixelCountingSemaphore.wait()
-		pixelCountingMutex.lock()
-		var shouldExit = exitThread
-		pixelCountingMutex.unlock()
-		
-		if shouldExit: break
-		
-		#Store locally to decrease the time locked
-		pixelCountingMutex.lock()
-		var localImageToCount: Image = imageToCount.duplicate()
-		pixelCountingMutex.unlock()
-		var asteroidMakeUp: Dictionary = {"r":0,"g":0,"b":0}
-		for i in range(0,300):
-			for j in range(0,300):
-				var testColor : Color = localImageToCount.get_pixel(i,j)
-				if (cmpColors(red,testColor)):
-					asteroidMakeUp["r"] += 1
-				if (cmpColors(green,testColor)):
-					asteroidMakeUp["g"] += 1
-				if (cmpColors(blue,testColor)):
-					asteroidMakeUp["b"] += 1
-
-		scoreMutex.lock()
-		for key in asteroidMakeUp.keys():
-			score[key] += asteroidMakeUp[key]
-			print(key, ": ", asteroidMakeUp[key])
-		scoreUpdate = true
-		scoreMutex.unlock()
 	
 func printAllPixels(myImage: Image):
 	var dict = {}
@@ -136,13 +52,3 @@ func printAllPixels(myImage: Image):
 		if dict[key] > 200:
 			print(key, ": ", dict[key])
 
-func cmpColors(x: Color, y: Color) -> bool:
-	if (x.a8 != y.a8):
-		return false
-	if (x.r8 != y.r8):
-		return false
-	if (x.g8 != y.g8):
-		return false
-	if (x.b8 != y.b8):
-		return false
-	return true
---
+++ Objects/pixel_counter/pixel_counter.gd
@@ -0,0 +1,111 @@
+extends SubViewport
+
+#region Variables for the pixel counting thread
+var pixelCountingThread: Thread
+var pixelCountingSemaphore: Semaphore
+var pixelCountingMutex: Mutex
+var exitThread: bool = false
+var imageToCount: Image
+#endregion
+
+var scoreMutex: Mutex
+var scoreUpdate: bool = false
+var score: Dictionary = {"r":0,"g":0,"b":0}
+
+#TODO: Update colour definitions when a new texture is made
+#region Asteroid Colors
+var red : Color = Color(1, 0, 0, 1)
+var green : Color = Color(0, 0.7569, 0.1255, 1)
+var blue : Color = Color(0, 0.5804, 1, 1)
+#endregion
+
+@onready var sub_viewport = $"."
+
+
+func _ready():
+	# Initialise all variables needed for the pixel counting thread
+	pixelCountingMutex = Mutex.new()
+	pixelCountingSemaphore = Semaphore.new()
+	pixelCountingThread = Thread.new()
+	pixelCountingThread.start(countRGBPixels)
+	exitThread = false
+	
+	scoreMutex = Mutex.new()
+
+func _exit_tree():
+	# Set exit condition to true.
+	pixelCountingMutex.lock()
+	exitThread = true 
+	pixelCountingMutex.unlock()
+
+	# Unblock by posting.
+	pixelCountingSemaphore.post()
+
+	# Wait until it exits.
+	pixelCountingThread.wait_to_finish()
+
+
+# Do we return a percent or do we simply return the count of all found colors
+
+## Passed a visual polygon which will be placed in the viewport to have the picture taken
+func visualPolygonImageGenerator(p: Polygon2D) -> void:
+	var ourPoly : Polygon2D = p.duplicate()
+	sub_viewport.add_child(ourPoly)
+	ourPoly.position = Vector2(150,150)
+	ourPoly.rotation = 0
+	
+	await RenderingServer.frame_post_draw
+	print("Attempting to make image")
+	var myImage : Image = sub_viewport.get_texture().get_image()
+	
+	pixelCountingMutex.lock()
+	imageToCount = myImage
+	pixelCountingMutex.unlock()
+	pixelCountingSemaphore.post()
+	
+	
+
+
+## NOTE: Variable passed to countRGBPixels by the imageToCount global variable
+## Threaded function which returns the red, green, blue pixel count of imageToCount
+func countRGBPixels() -> void:
+	while true:
+		pixelCountingSemaphore.wait()
+		pixelCountingMutex.lock()
+		var shouldExit = exitThread
+		pixelCountingMutex.unlock()
+		
+		if shouldExit: break
+		
+		#Store locally to decrease the time locked
+		pixelCountingMutex.lock()
+		var localImageToCount: Image = imageToCount.duplicate()
+		pixelCountingMutex.unlock()
+		var asteroidMakeUp: Dictionary = {"r":0,"g":0,"b":0}
+		for i in range(0,300):
+			for j in range(0,300):
+				var testColor : Color = localImageToCount.get_pixel(i,j)
+				if (cmpColors(red,testColor)):
+					asteroidMakeUp["r"] += 1
+				if (cmpColors(green,testColor)):
+					asteroidMakeUp["g"] += 1
+				if (cmpColors(blue,testColor)):
+					asteroidMakeUp["b"] += 1
+
+		scoreMutex.lock()
+		for key in asteroidMakeUp.keys():
+			score[key] += asteroidMakeUp[key]
+			print(key, ": ", asteroidMakeUp[key])
+		scoreUpdate = true
+		scoreMutex.unlock()
+
+func cmpColors(x: Color, y: Color) -> bool:
+	if (x.a8 != y.a8):
+		return false
+	if (x.r8 != y.r8):
+		return false
+	if (x.g8 != y.g8):
+		return false
+	if (x.b8 != y.b8):
+		return false
+	return true 
