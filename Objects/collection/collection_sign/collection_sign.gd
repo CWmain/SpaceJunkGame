14ef439385e1d20f8e5a440e54879fcbb62a23d5
CWmain <cameronwhite2478@gmail.com>
2025-01-17 13:28:03 +1100

    Have the score sign displaying the color it is collecting and the total spawned

--- Objects/collection/collection_sign/collection_sign.gd
+++ Objects/collection/collection_sign/collection_sign.gd
@@ -3,6 +3,11 @@ extends Polygon2D
 @onready var label = $Label
 
 @export var pixel_counter: SubViewport
+
+var totalSpawned: Dictionary = {"r":0,"g":0,"b":0}
+var collectedRed : int
+var collectedGreen : int
+var collectedBlue : int
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	assert(pixel_counter != null)
@@ -14,5 +19,18 @@ func _process(_delta):
 	pass
 
 func _on_score_signal(id: String, s : Dictionary):
-	var toDisplay: String = "From %s\n%s" % [id, str(s)]
+	if (id == "Spawner"):
+		for key in totalSpawned.keys():
+			totalSpawned[key] += s[key]
+	#TODO: Subtract score when wrong color in wrong section
+	elif(id == "Red"):
+		collectedRed += s["r"]
+	elif(id == "Green"):
+		collectedGreen += s["g"]
+	elif(id == "Blue"):
+		collectedBlue += s["b"]
+	
+	# Create the text to display
+	var toDisplay: String = "Total Spawned: %s\nRed: %s\nGreen: %s\nBlue: %s" % [str(s), collectedRed, collectedGreen, collectedBlue]
+	
 	label.set_text(toDisplay)
