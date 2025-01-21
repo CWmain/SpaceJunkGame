59241cd9c540721a030907362455d74bf730126f
CWmain <cameronwhite2478@gmail.com>
2025-01-21 16:20:32 +1100

    Have MM subtract score when wrong color is found

--- Objects/money_manager/money_manager.gd
+++ Objects/money_manager/money_manager.gd
@@ -6,6 +6,9 @@ signal scoreChanged(s: Dictionary)
 
 var score : int = 0
 
+# While testing will keep a history of the score gain / loss (0,1 respectivly)
+var scoreHistory : Dictionary = {"r": [0,0],"g": [0,0],"b": [0,0]}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect to each collectors signal
@@ -15,9 +18,25 @@ func _ready():
 func _on_score_signal(id, s):
 	if(id == "Red"):
 		score += s["r"]
+		score -= s["g"]
+		score -= s["b"]
+		scoreHistory["r"][0] += s["r"]
+		scoreHistory["g"][1] += s["g"]
+		scoreHistory["b"][1] += s["b"]
 	elif(id == "Green"):
+		score -= s["r"]
 		score += s["g"]
+		score -= s["b"]
+		scoreHistory["r"][1] += s["r"]
+		scoreHistory["g"][0] += s["g"]
+		scoreHistory["b"][1] += s["b"]
 	elif(id == "Blue"):
+		score -= s["r"]
+		score -= s["g"]
 		score += s["b"]
+		scoreHistory["r"][1] += s["r"]
+		scoreHistory["g"][1] += s["g"]
+		scoreHistory["b"][0] += s["b"]
 	print("MM working for %s" % [id])
+	print("MM: ScoreHistory %s" % scoreHistory)
 	scoreChanged.emit(score)
